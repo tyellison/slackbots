eecf917a3103baa95be97f4ed2d9b4f50f90a1c3
tyellison <tyellison05@hotmail.com>
2023-01-19 11:01:10 -0800

    adding daily update command

---
+++ messages.py
@@ -0,0 +1,4 @@
+Messages = {
+    "enabled" : "Sorry, it looks like daily updates are already enabled",
+    "disabled" : "Daily updates enabled"
+}
--- slackbot.py
+++ slackbot.py
@@ -1,11 +1,14 @@
-import os, time
+import os, time, datetime as dt
+from messages import Messages
+from threading import Lock
 from tabulate import tabulate
-from datetime import datetime as dt
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from pass_planner_helper import compute_passes
 
+
 DT_FORMAT = "%Y-%m-%d %H:%M"
+daily_update_lock = Lock()
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 ##### SLACK API CALLBACKS ##### 
@@ -17,10 +20,30 @@ def handle_app_mention_events(body, say):
 def handle_message_events(body, say):
     pass
 
-@app.command("/schedule")
+@app.command("/daily_update")
 def handle_daily_update_command(ack, body, say):
-    pass_table = get_pass_table()
-    weather = get_weather()
+    ack()
+
+    if daily_update_lock.acquire(blocking=False) == False:
+        say(Messages["enabled"])
+        return
+
+    say(Messages["disabled"])
+
+    tgt = dt.datetime(2023, 1, 1, 6, 0, 0)
+    while True:
+        now = dt.datetime.now()
+        tgt = dt.datetime.combine(now.date(), tgt.time())
+        td = tgt - now
+
+        if td.days < 0:
+            tgt += dt.timedelta(days=1)
+            td = tgt - now
+
+        time.sleep(td.total_seconds())
+        #weather = get_weather()
+        pass_table = get_pass_table()    
+        say(pass_table)
 
 @app.command("/pass_info")
 def handle_pass_info_command(ack, body, say):
@@ -36,11 +59,11 @@ def handle_recur_command(ack, body, say):
 def get_pass_table():
     passes = compute_passes()
     passes = [pi_obj for pi_obj in passes if pi_obj.max_alt_deg >= 15.0]
-    passes = [ \
-                [pi.local_start_time.strftime(DT_FORMAT), \
-                round(pi.max_alt_deg, 0), \
-                round(pi.duration.total_seconds(), 0)] \
-                for pi in passes \
+    passes = [ 
+                [pi.local_start_time.strftime(DT_FORMAT), 
+                round(pi.max_alt_deg, 0), 
+                round(pi.duration.total_seconds(), 0)] 
+                for pi in passes 
              ]
     pass_table_headers = ["Start Time (PST)", "Max Elev (deg)", "Duration (sec)"]
     pass_table = tabulate(passes, headers=pass_table_headers, tablefmt="plain", numalign="right", stralign="left")
